437220bb0537a5c1cd6afbaa7027f9c865bd8c43
David Richey <darichey1@gmail.com>
2021-02-24 15:28:39 -0600

    Add documentation

---
+++ README.md
@@ -0,0 +1,44 @@
+# calendar-converter
+![](https://github.com/acmutd/calendar-converter/actions/workflows/refresh.yml/badge.svg)
+
+A script to automatically convert ACM UTD's event spreadsheet to the [iCalendar](https://wikipedia.org/wiki/ICalendar) format, so it can be displayed as a calendar (like in someone's Google Calendar or on [acmutd.co/events](https://acmutd.co/events)).
+
+## How it works
+Using [GitHub Actions](./.github/workflows/refresh.yml), this [script](./src/index.ts) runs every 24 hours. The core of the script is given by the few lines at the end:
+```ts
+fetchSpreadsheet()
+  .then(spreadsheetToEvents)
+  .then(eventsToIcs)
+  .then(console.log)
+```
+It is a pipeline that
+
+1. Fetches the spreadsheet data using the [Google Sheets API](https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets/get)
+2. Converts the raw data (which is just `any[][]`) to a more structured schema (`Event[]`)
+3. Produces a string in the iCalendar format from that array of `Event`s using the [ics package](https://www.npmjs.com/package/ics)
+4. Sends that string to stdout
+
+The output of the script is redirected to a file which is then committed and pushed to the [`ics` branch](https://github.com/acmutd/calendar-converter/tree/ics) of this repository. The calendar can then be accessed with the [raw link](https://raw.githubusercontent.com/acmutd/calendar-converter/ics/calendar.ics) to that file (which we have a vanity link for: `https://content.acmutd.co/events`).
+
+The remaining details are explained in comments in the source code.
+
+## Set up
+For the script to run correctly, some set up is necessary in both Google Workspace (G Suite) and GitHub Actions.
+
+### Google Workspace
+The script fetches data using the Google Spreadsheet API. As such, it requires authorization to access the spreadsheet. This requires
+
+1. A GCP project with the Google Sheets API [enabled](https://cloud.google.com/endpoints/docs/openapi/enable-api) (note for ACM officers: this is the "Calendar Converter" project)
+2. A [service account](https://cloud.google.com/iam/docs/service-accounts) on that project
+3. Sharing the spreadsheet with the service account (the service account has an email address which you can share to like normal)
+
+### GitHub Actions
+The script utilizes two [Environment secrets](https://docs.github.com/en/actions/reference/encrypted-secrets) which must be set before running: 
+
+* `EVENT_SPREADSHEET_ID`: The ID of the event spreadsheet. This can be found in the URL to the sheet (`https://docs.google.com/spreadsheets/d/THIS_IS_THE_ID/`)
+* `GCP_SA_KEY`: The service account [authentication key](https://cloud.google.com/iam/docs/creating-managing-service-account-keys#creating_service_account_keys). It should be the raw JSON string (i.e., not a file name). See [`setup-gcloud`](https://github.com/google-github-actions/setup-gcloud) for more info.
+
+## Local Development
+Clone the repo, run `npm install`, build with `npm run build`, run with `npm run start`. It might be more convenient to run the compiler in watch mode which can be done with `npm run build -- --watch`.
+
+The same Google Workspace set up process described above is required. The `EVENT_SPREADSHEET_ID` environment variable is the same, but Google API authentication will work differently. Instead of the `GCP_SA_KEY` variable, export a `GOOGLE_APPLICATION_CREDENTIALS` variable which has that path to the service account key json file (as opposed to the `GCP_SA_KEY` variable, this _is_ a file path, not the raw JSON string).
--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,23 @@ import { google } from "googleapis";
 import { createEvents } from "ics";
 import { arrayEquals, to24hour, toDateArray } from "./util";
 
+/**
+ * This is what we expect the first row of the spreadsheet to be. We use this as
+ * a sanity check in `spreadsheetToEvents` to make sure that the spreadsheet
+ * format hasn't changed before we try manipulating things.
+ */
 const EXPECTED_COLUMNS = ["Date", "End Date", "Day of Week", "Start Time", "End Time", "Name", "Description", "Location", "Division", "Collaborator(s)", "Public"] as const;
+/**
+ * The union of all of the strings in EXPECTED_COLUMNS (i.e., "Date" | "End Date" | ...).
+ * This allows us to ensure we don't accidentally use non-existant column name.
+ */
 type Column = typeof EXPECTED_COLUMNS[number];
 
+/**
+ * Our event schema. Each field corresponds to a column of the spreadsheet.
+ * Note that not all columns are present, because only some are necessary for
+ * generating the ics.
+ */
 interface Event {
   start: ZonedDateTime;
   end: ZonedDateTime;
@@ -15,8 +29,14 @@ interface Event {
   public: boolean;
 }
 
-// Fetch spreadsheet as any[][]
+/**
+ * Fetches the spreadsheet using the Google Sheets API. This yields the
+ * spreadsheet as a very unstructured `any[][]`.
+ */
 async function fetchSpreadsheet(): Promise<any[][]> {
+  // Construct a sheets client with the appropriate scope for reading
+  // spreadsheets. We don't pass any API key, because that is automatically
+  // detected from the `GOOGLE_APPLICATIONS_CREDENTIALS` env var.
   const sheets = google.sheets({
     version: 'v4',
     auth: new google.auth.GoogleAuth({
@@ -24,20 +44,40 @@ async function fetchSpreadsheet(): Promise<any[][]> {
     })
   });
 
+  // Request the spreadsheet. Passing 'Events' for the range fetches the entire
+  // sheet (which is called 'Events').
   const res = await sheets.spreadsheets.values.get({
     spreadsheetId: process.env.EVENT_SPREADSHEET_ID,
     range: 'Events'
   });
 
+  // Return the values of the sheet, coercing null | undefined to []
+  // (I don't actually know in what cases it would be null or undefined...).
   return res.data.values ?? [];
 }
 
-// Convert a single row of the spreadsheet to our Event model
+/**
+ * Converts a single row of the spreadsheet to an `Event`.
+ * @param row A row of the spreadsheet (an element of the result of `fetchSpreadsheet()`).
+ */
 function rowToEvent(row: any[]): Event {
+  /**
+   * Convenience function for getting a value from the row given a column name
+   * (e.g., `getValue("Date")` would get the first element of `row`).
+   * @param column The name of the column.
+   */
   function getValue(column: Column): any {
     return row[EXPECTED_COLUMNS.indexOf(column)];
   }
 
+  /**
+   * Combines the (End) Date and (End/Start) Time columns to form a `ZonedDateTime`.
+   * All times are parsed in the `America/Chicago` timezone and then converted
+   * to UTC to ensure consistent behavior when running the script in different
+   * timezones (See `eventsToIcs` for more info).
+   * @param dateCol The column from which to get the date part.
+   * @param timeCol The column from which to get the time part.
+   */
   function parseDate(dateCol: Column, timeCol: Column): ZonedDateTime {
     const localDate = LocalDate.parse(getValue(dateCol));
     const localTime = LocalTime.parse(to24hour(getValue(timeCol)));
@@ -54,26 +94,44 @@ function rowToEvent(row: any[]): Event {
   }
 }
 
-// Convert the whole spreadsheet to an array of Event
+/**
+ * Converts the raw spreadsheet data to an array of `Event`.
+ * @param spreadsheet The raw spreadsheet data (probably from `fetchSpreadsheet()`).
+ */
 function spreadsheetToEvents(spreadsheet: any[][]): Event[] {
+  // Sanity check that the header row is what we expect. See `EXPECTED_COLUMNS`.
   if (!arrayEquals(spreadsheet[0], EXPECTED_COLUMNS)) {
     throw new Error("Unexpected header, aborting: " + spreadsheet[0])
   }
 
-  spreadsheet.shift();
+  spreadsheet.shift(); // discard header row
   return spreadsheet.map(rowToEvent);
 }
 
-// Convert array of Event to ics string
-function toIcs(events: Event[], includePrivate: boolean = false): string {
+/**
+ * Converts the array of `Event`s to an ics string
+ * @param events The events to convert
+ * @param includePrivate Whether to include events not marked as public
+ */
+function eventsToIcs(events: Event[], includePrivate: boolean = false): string {
+  // If `includePrivate`, then take all the events, otherwise filter to only
+  // public events
   const includedEvents = includePrivate ? events : events.filter(e => e.public);
+
+  // Convert our Event schema to the ics package's `EventAttributes` schema
+  // and pass them to `createEvents` to produce the final ics
   const { error, value } = createEvents(includedEvents.map(e => {
     return {
-      calName: "ACM Events",
+      calName: "ACM Events", // The name of the calendar. idk why this is in every EventAttributes
       start: toDateArray(e.start),
       end: toDateArray(e.end),
       title: e.name,
       description: e.description,
+      // We do everything in utc to ensure consistent behvior regardless of the
+      // machine timezone. If the inputs were 'local', the ics package would
+      // always interpret it as being the local time of the machine, which isn't
+      // necessarily the case. We know the timezone of our events is always
+      // America/Chicago, so we did the conversion to UTC ourselves in `parseDate`.
       startInputType: 'utc',
       endInputType: 'utc',
       startOutputType: 'utc',
@@ -81,13 +139,16 @@ function toIcs(events: Event[], includePrivate: boolean = false): string {
     };
   }));
 
+  // It does this weird `ReturnObject` thing instead of throwing an error, so we
+  // just throw it if the value is null.
   if (!value) throw error;
   return value;
 }
 
+// Tie everything together and send the result to stdout (or log an error).
 fetchSpreadsheet()
   .then(spreadsheetToEvents)
-  .then(toIcs)
+  .then(eventsToIcs)
   .then(console.log)
   .catch(r => {
     console.error(`Error: ${r}`);
